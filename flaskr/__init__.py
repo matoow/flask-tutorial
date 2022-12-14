880cb4663ebbc2b2700ed05701eb20334cb34fa6
Matt Williams <matoow@gmail.com>
2022-12-15 03:35:26 +0800

    flask-cognito

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,28 +3,28 @@ from flask import Flask
 
 # moving to https://pypi.org/project/Flask-Cognito/
 
-# from flask_awscognito import AWSCognitoAuthentication
+global cogauth
 
-# COGNITO_CONFIG = dict(
-#     AWS_DEFAULT_REGION='ap-northeast-1',
-#     AWS_COGNITO_DOMAIN='localhost',
-#     AWS_COGNITO_USER_POOL_ID='ap-northeast-1_Yt2TJKlcN',
-#     AWS_COGNITO_USER_POOL_CLIENT_ID='4j2nh0nonrto9cadeic7m0phjn',
-#     AWS_COGNITO_USER_POOL_CLIENT_SECRET='1ev8jmho7aacfd91l2ga4a44e5tml9e2emfdncabg2m614ujqhbp',
-#     AWS_COGNITO_REDIRECT_URL='http://localhost:5000/aws_cognito_redirect',
-# )
+COGNITO_CONFIG = dict(
+    COGNITO_REGION='ap-northeast-1',
+    COGNITO_DOMAIN='localhost',
+    COGNITO_USERPOOL_ID='ap-northeast-1_Yt2TJKlcN',
+    COGNITO_APP_CLIENT_ID='4j2nh0nonrto9cadeic7m0phjn',
+    # COGNITO_APP_CLIENT_SECRET='1ev8jmho7aacfd91l2ga4a44e5tml9e2emfdncabg2m614ujqhbp',
+    COGNITO_REDIRECT_URL='http://localhost:5000/aws_cognito_redirect',
+)
 
 
 def create_app(test_config=None):
+    global cogauth
+
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
+    app.config.from_mapping(dict(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    #    **COGNITO_CONFIG
-    )
-
-    # app.cognito = AWSCognitoAuthentication(app)
+        **COGNITO_CONFIG
+    ))
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -32,7 +32,6 @@ def create_app(test_config=None):
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
-
 
     # ensure the instance folder exists
     try:
@@ -52,6 +51,7 @@ def create_app(test_config=None):
     app.register_blueprint(auth.bp)
 
     from . import aws_auth
+    aws_auth.init(app)
     app.register_blueprint(aws_auth.bp)
 
     from . import blog
--- flaskr/aws_auth.py
+++ flaskr/aws_auth.py
@@ -7,9 +7,22 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from flaskr.db import get_db
 
+from flask_cognito import CognitoAuth
+
+cogauth = CognitoAuth()
+
+def init(app):
+    cogauth.init_app(app)
+
+@cogauth.identity_handler
+def lookup_cognito_user(payload):
+    """Look up user in our database from Cognito JWT payload."""
+    print(payload)
+    # return User.query.filter(User.cognito_username == payload['username']).one_or_none()
+
 
 bp = Blueprint('aws', __name__, url_prefix='/aws')
 
 @bp.route('/login')
 def login():
-    return redirect(g.cognito.get_sign_in_url())
+    return redirect(cogauth.get_sign_in_url())
